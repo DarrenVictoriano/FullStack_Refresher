82db5f0e29548f375616d69376af1fc1b654120b
Darren Victoriano <darren.victoriano@gmail.com>
2020-02-17 18:05:58 -0800

    react shoppinglist done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          FullStack Refresher
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    );
+  }
 }
 
 export default App;
